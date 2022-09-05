aff70475a5067f496d5a4c8ed4089f73962064f2
nich-nichy <78273608+nich-nichy@users.noreply.github.com>
2022-09-05 20:55:26 +0530

    new code

--- main.py
+++ main.py
@@ -786,9 +786,44 @@
 # print("this", reverseInGroups(arr, N, K))
 
 
+# Program for spiral order
+# class Solution:
+#     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+#         res = []
+#         left, right = 0, len(matrix[0])
+#         top, bottom = 0, len(matrix)
+#
+#         while left < right and top < bottom:
+#             for i in range(left, right):
+#                 res.append(matrix[top][i])
+#             top += 1
+#
+#             for i in range(top, bottom):
+#                 res.append(matrix[i][right - 1])
+#             right -= 1
+#
+#             if not (left < right and top < bottom):
+#                 break
+#
+#             for i in range(right - 1, left - 1, -1):
+#                 res.append(matrix[bottom - 1][i])
+#             bottom -= 1
+#
+#             for i in range(bottom - 1, top - 1, -1):
+#                 res.append(matrix[i][left])
+#             left += 1
+#         return res
 
 
-
-
-
+def findDups(nums):
+    for i in range(0, len(nums)):
+        if nums[i] == nums[i]:
+            arr2 = []
+            arr2.append(nums[i])
+            print(arr2)
+            return True
+        else:
+            return False
+nums = [1, 2, 3, 1]
+print(findDups(nums))
 
